1f7e34aedcfc6742a54b7216588221ee578c9391
shivam0626 <shivammaurya7310@gmail.com>
2023-03-27 21:14:54 +0530

    backend folder  initiated

---
+++ whistler-backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const rooms = ['general','finance','tech','crypto'];
+const cors = require('cors');
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cors());
+
+const server = require('http').createServer(app);
+const PORT = 8000;
+const io = require('socket.io')(server,{
+    cors:{
+        origin: 'http://localhost:3000',
+        methods: ['GET','POST']
+    }
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Listening to port ${PORT}`);
+})
+
