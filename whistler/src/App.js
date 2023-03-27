27dd1b4e9dff13bab240707c169884db87a8f08e
shivam0626 <shivammaurya7310@gmail.com>
2023-03-27 22:43:07 +0530

    pages and navigation initialized

--- whistler/src/App.js
+++ whistler/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Navigation from './components/Navigation';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Chat from './pages/Chat';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+       <Navigation />
+       <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element ={<Signup />} />
+          <Route path='/chat' element={<Chat />} />
+       </Routes>
+    </BrowserRouter>
   );
 }
 
