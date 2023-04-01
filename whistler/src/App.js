93c1b558ae9dc8859aa124b2e74d94943387364b
shivam0626 <shivammaurya7310@gmail.com>
2023-04-01 23:19:54 +0530

    hide conditional components frontend

--- whistler/src/App.js
+++ whistler/src/App.js
@@ -7,16 +7,22 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Chat from './pages/Chat';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const user = useSelector((state)=>state.user);
   return (
     <div className='App'>
       <BrowserRouter>
        <Navigation />
        <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element ={<Signup />} />
+          { !user &&(
+            <>  
+              <Route path='/login' element={<Login />} />
+              <Route path='/signup' element ={<Signup />} />
+            </>
+          )}
           <Route path='/chat' element={<Chat />} />
        </Routes>
       </BrowserRouter>
