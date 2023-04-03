cee2e653aa3e475379fb4e1640fc7f2112e5327d
shivam0626 <shivammaurya7310@gmail.com>
2023-04-03 22:43:20 +0530

    socket connection created

--- whistler-backend/server.js
+++ whistler-backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const userRoutes = require("./routes/userRoutes");
 const cors = require('cors');
+const Message = require('./models/Message');
+const User = require('./models/User');
 
 const rooms = ['general','finance','tech','crypto'];
 
@@ -26,12 +28,41 @@ app.get('/rooms',(req,res)=> {
     res.json(rooms);
 })
 
+async function getLastMessagesFromRoom(room){
+    let roomMessages = await Message.aggregate([
+        {$match: {to: room } },
+        {$group: {_id:'date', messagesByDate: {$push: '$$ROOT'}}}
+    ])
+    return roomMessages;
+}
+
+function sortRoomMessagesByDate(messages){
+    return messages.sort(function(a,b){
+        let date1 = a._id.split('/');
+        let date2 = a._id.split('/');
+
+        date1 = date1[2] + date1[0] + date1[1];
+        date2 = date2[2] + date2[0] + date2[1];
+
+        return date1 < date2 ? -1 : 1;
+    })
+    
+}
+
 // socket connection
 
 io.on('connection',(socket)=>{
+
+    socket.on('new-user',async()=>{
+        const members = await User.find();
+        io.emit('new-user',members)
+    })
+
     socket.on('join-room',async(room)=>{
         socket.join(room);
-        
+        let roomMessages = await getLastMessagesFromRoom(room);
+        roomMessages = sortRoomMessagesByDate(roomMessages);
+        socket.emit('room-messages',roomMessages)
     })
 })
 
