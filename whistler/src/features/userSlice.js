2fd898df05ee027834512f0ac6461cf4d7d47b89
shivam0626 <shivammaurya7310@gmail.com>
2023-04-16 22:02:00 +0530

    handle notifications done

--- whistler/src/features/userSlice.js
+++ whistler/src/features/userSlice.js
@@ -5,7 +5,16 @@ export const userSlice = createSlice({
     name:'user',
     initialState: null,
     reducers:{
-      
+        addNotifications:(state,{payload})=>{
+            if(state.newMessages[payload]){
+                state.newMessages[payload] = state.newMessages[payload] + 1;
+            } else{
+                state.newMessages[payload] = 1;
+            }
+        },
+        resetNotifications:(state,{payload})=>{
+            delete state.newMessages[payload];
+        },
     },
     extraReducers: (builder) =>{
         // save user after signup
