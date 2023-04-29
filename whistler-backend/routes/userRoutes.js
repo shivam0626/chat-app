5587786757d71441fbf0bb39b80b10bd6833bac8
shivam0626 <shivammaurya7310@gmail.com>
2023-04-29 13:03:28 +0530

    users route updated

--- whistler-backend/routes/userRoutes.js
+++ whistler-backend/routes/userRoutes.js
@@ -27,9 +27,9 @@ router.get('/getuser', async(req,res)=> {
     try {
         const { email, password } = req.body;
         const user = await User.findByCredentials(email,password);
-        user.status = 'online';
-        await user.save();
-        res.status(200).json(user);
+        // user.status = 'online';
+        // await user.save();
+        res.status(200).send(json(user));
     } catch (e) {
         res.status(400).json(e.message);
     }
